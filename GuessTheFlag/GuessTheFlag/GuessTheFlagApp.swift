7dd3c1e36c792edad14b1c40e73d4d691590c1f3
Anmol Chawla <anmolchawla10@gmail.com>
2023-06-24 17:49:05 +0530

    Init GuessTheFlag project

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Anmol Chawla on 03/06/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
