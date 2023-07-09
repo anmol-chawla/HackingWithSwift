424bb0be4ec168ae8c3a6ce25be77d2d94be2c02
Anmol Chawla <anmolchawla10@gmail.com>
2023-07-09 18:00:15 +0530

    Add scoring

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,9 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
-    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria",
-                     "Poland", "Russia", "Spain", "UK", "US"]
-    var correctAnswer = Int.random(in: 0...2)
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    @State private var score = 0
+
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria",
+                     "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+            score += 1
+        } else {
+            scoreTitle = "Wrong"
+        }
+        showingScore = true
+    }
+
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
 
     var body: some View {
         ZStack {
@@ -23,17 +42,21 @@ struct ContentView: View {
                 Text(countries[correctAnswer])
                     .foregroundColor(.white)
             }
-            
+
             ForEach(0..<3) { number in
                 Button {
-                    
+                    flagTapped(number)
                 } label: {
                     Image(countries[number])
                         .renderingMode(.original)
+                    }
                 }
             }
         }
-            
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text("Your score is \(score)")
         }
     }
 }
