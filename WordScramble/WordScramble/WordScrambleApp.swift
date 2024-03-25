3fde7011646a0c341509f03537bca10e1a29cfcf
Anmol Chawla <anmolchawla10@gmail.com>
2024-03-25 19:02:33 +0530

    Project 5: Word Scramble

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Anmol Chawla on 25/03/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
