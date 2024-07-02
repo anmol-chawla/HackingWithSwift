90d933b7fcb78d14b3dfed14acbe3739f951c14b
Anmol Chawla <anmolchawla10@gmail.com>
2024-07-02 10:11:02 +0530

    Experimenting with overlays and onAppear

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -12,14 +12,26 @@ struct ContentView: View {
     
     var body: some View {
         Button("Tap Me") {
-            animationAmount += 1
         }
         .padding(50)
         .background(.red)
         .foregroundColor(.white)
         .clipShape(.circle)
-        .scaleEffect(animationAmount)
-        .animation(.default, value: animationAmount)
+        .overlay(
+            Circle()
+                .stroke(.red)
+                .scaleEffect(animationAmount)
+                .opacity(2 - animationAmount)
+                .animation(
+                    .easeOut(duration: 1)
+                    .repeatForever(autoreverses: false),
+                    value: animationAmount
+                )
+            
+        )
+        .onAppear {
+            animationAmount = 2
+        }
     }
 }
 
