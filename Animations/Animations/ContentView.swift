9b8cc34a3227a8f5af02a8c13ca4d0c8f846e39c
Anmol Chawla <anmolchawla10@gmail.com>
2024-07-04 09:47:39 +0530

    Playing with animation() binding

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -9,28 +9,46 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var animationAmount = 1.0
+    @State private var animationAmount2 = 1.0
     
     var body: some View {
-        Button("Tap Me") {
-        }
-        .padding(50)
-        .background(.red)
-        .foregroundColor(.white)
-        .clipShape(.circle)
-        .overlay(
-            Circle()
-                .stroke(.red)
-                .scaleEffect(animationAmount)
-                .opacity(2 - animationAmount)
-                .animation(
-                    .easeOut(duration: 1)
-                    .repeatForever(autoreverses: false),
-                    value: animationAmount
-                )
+        print(animationAmount2)
+        
+        return VStack {
+            Button("Tap Me") {
+            }
+            .padding(50)
+            .background(.red)
+            .foregroundColor(.white)
+            .clipShape(.circle)
+            .overlay(
+                Circle()
+                    .stroke(.red)
+                    .scaleEffect(animationAmount)
+                    .opacity(2 - animationAmount)
+                    .animation(
+                        .easeOut(duration: 1)
+                        .repeatForever(autoreverses: false),
+                        value: animationAmount
+                    )
+                
+            )
+            .onAppear {
+                animationAmount = 2
+            }
             
-        )
-        .onAppear {
-            animationAmount = 2
+            Stepper("Scale amount", value: $animationAmount2.animation(), in: 1...10)
+            
+            Spacer()
+            
+            Button("Tap Me") {
+                animationAmount2 += 1
+            }
+            .padding(40)
+            .background(.red)
+            .foregroundColor(.white)
+            .clipShape(.circle)
+            .scaleEffect(animationAmount2)
         }
     }
 }
