3ca68ea09bc141a894201927e26276421ee5462c
Anmol Chawla <anmolchawla10@gmail.com>
2024-07-06 17:37:38 +0530

    Playing with transitions and custom transitions

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -7,6 +7,26 @@
 
 import SwiftUI
 
+struct CornerRotateModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
+    
+    func body(content: Content) -> some View {
+        content
+            .rotationEffect(.degrees(amount), anchor: anchor)
+            .clipped()
+    }
+}
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(
+            active: CornerRotateModifier(amount: -90, anchor: .topLeading),
+            identity: CornerRotateModifier(amount: 0, anchor: .topLeading)
+        )
+    }
+}
+
 struct ContentView: View {
     let letters = Array("Hello, SwiftUI")
     
@@ -17,34 +37,12 @@ struct ContentView: View {
     @State private var dragAmount = CGSize.zero
     @State private var lettersEnabled = false
     @State private var lettersDragAmount = CGSize.zero
+    @State private var isShowing = false
     
     var body: some View {
         print(animationAmount2)
         
         return VStack {
-            Button("Tap Me") {
-            }
-            .padding(50)
-            .background(.red)
-            .foregroundColor(.white)
-            .clipShape(.circle)
-            .overlay(
-                Circle()
-                    .stroke(.red)
-                    .scaleEffect(animationAmount)
-                    .opacity(2 - animationAmount)
-                    .animation(
-                        .easeOut(duration: 1)
-                        .repeatForever(autoreverses: false),
-                        value: animationAmount
-                    )
-                
-            )
-            .onAppear {
-                animationAmount = 2
-            }
-            
-            Spacer()
             
             Stepper("Scale amount", value: $animationAmount2.animation(), in: 1...10)
             
@@ -113,13 +111,29 @@ struct ContentView: View {
                         .animation(.linear.delay(Double(num)/20), value: lettersDragAmount)
                 }
             }
-            .gesture(DragGesture()
+            .gesture(
+                DragGesture()
                 .onChanged { lettersDragAmount = $0.translation }
                 .onEnded { _ in
                     lettersDragAmount = .zero
                     lettersEnabled.toggle()
                 }
-                     )
+            )
+            
+            Spacer()
+            
+            Button("Tap Me") {
+                withAnimation {
+                    isShowing.toggle()
+                }
+            }
+            
+            if isShowing {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
+            }
         }
     }
 }
