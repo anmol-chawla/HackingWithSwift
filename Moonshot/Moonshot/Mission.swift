a97933f24dd12f87f01db99a0c934d4b8e14d711
Anmol Chawla <anmolchawla10@gmail.com>
2024-07-27 17:37:18 +0530

    Add stuct for mission object

---
+++ Moonshot/Moonshot/Mission.swift
@@ -0,0 +1,21 @@
+//
+//  Mission.swift
+//  Moonshot
+//
+//  Created by Anmol Chawla on 27/07/24.
+//
+
+import Foundation
+
+
+struct Mission: Codable, Identifiable {
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+    
+    let id: Int
+    let launchDate: String?
+    let crew: [CrewRole]
+    let description: String
+}
