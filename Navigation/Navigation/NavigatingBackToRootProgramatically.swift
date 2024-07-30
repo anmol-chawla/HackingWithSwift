d285dd86a5193f914fb737944050b63b364e7296
Anmol Chawla <anmolchawla10@gmail.com>
2024-07-30 10:15:31 +0530

    Adding a class to handle storing and reloading the navigation state

--- Navigation/Navigation/NavigatingBackToRootProgramatically.swift
+++ Navigation/Navigation/NavigatingBackToRootProgramatically.swift
@@ -7,6 +7,39 @@
 
 import SwiftUI
 
+@Observable
+class PathStore {
+    var path: NavigationPath {
+        didSet {
+            save()
+        }
+    }
+    
+    private let savePath = URL.documentsDirectory.appending(path: "SavedPath")
+    
+    init() {
+        if let data = try? Data(contentsOf: savePath) {
+            if let decoded = try? JSONDecoder().decode(NavigationPath.CodableRepresentation.self, from: data) {
+                path = NavigationPath(decoded)
+                return
+            }
+        }
+        
+        path = NavigationPath()
+    }
+    
+    func save() {
+        guard let representation = path.codable else { return }
+        
+        do {
+            let data = try JSONEncoder().encode(representation)
+            try data.write(to: savePath)
+        } catch {
+            print("Failed to save navigation data")
+        }
+    }
+}
+
 struct DetailView: View {
     var number: Int
     @Binding var path: NavigationPath
@@ -23,13 +56,13 @@ struct DetailView: View {
 }
 
 struct NavigatingBackToRootProgramatically: View {
-    @State private var path = NavigationPath()
+    @State private var pathStore = PathStore()
     
     var body: some View {
-        NavigationStack(path: $path, root: {
-            DetailView(number: 0, path: $path)
+        NavigationStack(path: $pathStore.path, root: {
+            DetailView(number: 0, path: $pathStore.path)
                 .navigationDestination(for: Int.self, destination: { i in
-                    DetailView(number: i, path: $path)
+                    DetailView(number: i, path: $pathStore.path)
                 })
         })
     }
