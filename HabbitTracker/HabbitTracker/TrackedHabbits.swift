656706b957d4919646e1bec8f3d70e4189f93862
Anmol Chawla <anmolchawla10@gmail.com>
2024-08-04 20:20:30 +0530

    Add class to track list of habbits

---
+++ HabbitTracker/HabbitTracker/TrackedHabbits.swift
@@ -0,0 +1,30 @@
+//
+//  TrackedHabbits.swift
+//  HabbitTracker
+//
+//  Created by Anmol Chawla on 04/08/24.
+//
+
+import Foundation
+
+@Observable
+class TrackedHabbits: Codable {
+    var id: UUID = UUID()
+    var habbits: [Habbit] {
+        didSet {
+            if let encoded = try? JSONEncoder().encode(habbits) {
+                UserDefaults.standard.setValue(encoded, forKey: "habbits")
+            }
+        }
+    }
+    
+    init() {
+        if let savedItems = UserDefaults.standard.data(forKey: "habbits") {
+            if let decodedItems = try? JSONDecoder().decode([Habbit].self, from: savedItems) {
+                habbits = decodedItems
+                return
+            }
+        }
+        habbits = []
+    }
+}
