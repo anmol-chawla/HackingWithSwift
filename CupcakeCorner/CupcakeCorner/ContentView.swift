cfd9c2ed5346f4db8bc6d6c3b8eae082c7bcd741
Anmol Chawla <anmolchawla10@gmail.com>
2024-08-10 19:12:44 +0530

    Add changes for project

--- CupcakeCorner/CupcakeCorner/ContentView.swift
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -8,14 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var order = Order()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+       NavigationStack(root: {
+           Form(content: {
+               Section(content: {
+                   Picker(
+                    "Select your cake type",
+                    selection: $order.type,
+                    content: {
+                        ForEach(Order.types.indices, id: \.self, content: {
+                            Text(Order.types[$0])
+                        })
+                    }
+                  )
+                   
+                   Stepper("Number of cakes: \(order.quantity)", value: $order.quantity, in: 3...20)
+               })
+               
+               Section(content: {
+                   Toggle(
+                    "Any special requests?",
+                    isOn: $order.specialRequestsEnabled
+                   )
+                   
+                   if order.specialRequestsEnabled {
+                       Toggle(
+                        "Add extra frosting",
+                        isOn: $order.extraFrosting
+                       )
+                       
+                   Toggle(
+                        "Add extra sprinkles",
+                        isOn: $order.addSprinkles
+                       )
+                   }
+               })
+               
+               Section(content: {
+                   NavigationLink("Delivery Details", destination: {
+                       AddressView(order: order)
+                   })
+               })
+           })
+           .navigationTitle("Cupcake Corner")
+       })
     }
 }
 
