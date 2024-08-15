b1cdb411ce3b731623758eea6e2b8eb2b8b3811b
Anmol Chawla <anmolchawla10@gmail.com>
2024-08-15 12:25:49 +0530

    Add simple view for reviews created

--- Bookworm/Bookworm/ContentView.swift
+++ Bookworm/Bookworm/ContentView.swift
@@ -5,17 +5,34 @@
 //  Created by Anmol Chawla on 13/08/24.
 //
 
+import SwiftData
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.modelContext) var modelContext
+    @Query var books: [Book]
+    
+    @State private var showingAddScreen = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationStack(root: {
+            Text("Count : \(books.count)")
+                .navigationTitle("Bookworm")
+                .toolbar(content: {
+                    ToolbarItem(placement: .topBarTrailing, content: {
+                        Button(
+                            "Add Book",
+                            systemImage: "plus",
+                            action: {
+                                showingAddScreen.toggle()
+                            }
+                       )
+                    })
+                })
+        })
+        .sheet(isPresented: $showingAddScreen, content: {
+            AddBookView()
+        })
     }
 }
 
