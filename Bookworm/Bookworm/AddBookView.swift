252443523de49dfe039f0bce27a013037d236fe0
Anmol Chawla <anmolchawla10@gmail.com>
2024-08-15 12:19:33 +0530

    Add a view for adding a book review

--- Bookworm/Bookworm/AddBookView.swift
+++ Bookworm/Bookworm/AddBookView.swift
@@ -8,8 +8,49 @@
 import SwiftUI
 
 struct AddBookView: View {
+    @Environment(\.modelContext) var modelContext
+    
+    @State private var title = ""
+    @State private var author = ""
+    @State private var rating = 3
+    @State private var genre = "Fantasy"
+    @State private var review = ""
+    
+    let genres = ["Fantasy", "Horror", "Kids", "Mystery", "Poetry", "Romance", "Thriller"]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack(root: {
+            Form(content: {
+                Section(content: {
+                    TextField("Name of the book", text: $title)
+                    TextField("Author's name", text: $author)
+                    
+                    Picker("Genre", selection: $genre, content: {
+                        ForEach(genres, id: \.self, content: {
+                            Text($0)
+                        })
+                    })
+                })
+                
+                Section("Write a review", content: {
+                    TextEditor(text: $review)
+                    
+                    Picker("Rating", selection: $rating, content: {
+                        ForEach(0..<6, content: {
+                            Text(String($0))
+                        })
+                    })
+                })
+                
+                Section(content: {
+                    Button("Save", action: {
+                        let newBook = Book(title: title, author: author, genre: genre, review: review, rating: rating)
+                        modelContext.insert(newBook)
+                    })
+                })
+            })
+            .navigationTitle("Add Book")
+        })
     }
 }
 
