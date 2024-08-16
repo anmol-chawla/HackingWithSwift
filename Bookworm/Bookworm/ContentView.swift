44948212512dbde819776cb038d57c071ae91ee4
Anmol Chawla <anmolchawla10@gmail.com>
2024-08-16 18:18:18 +0530

    Implement sorting for the review order

--- Bookworm/Bookworm/ContentView.swift
+++ Bookworm/Bookworm/ContentView.swift
@@ -10,7 +10,13 @@ import SwiftUI
 
 struct ContentView: View {
     @Environment(\.modelContext) var modelContext
-    @Query var books: [Book]
+    @Query(
+        sort: [
+            SortDescriptor(\Book.rating, order: .reverse),
+            SortDescriptor(\Book.title),
+            SortDescriptor(\Book.author)
+        ]
+    ) var books: [Book]
     
     @State private var showingAddScreen = false
     
