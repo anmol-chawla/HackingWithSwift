9f013bde0d8020d66c18fe74838ff7c1ca896056
Anmol Chawla <anmolchawla10@gmail.com>
2024-08-16 17:51:04 +0530

    Add a component to show emoji for rating

---
+++ Bookworm/Bookworm/EmojiRatingView.swift
@@ -0,0 +1,38 @@
+//
+//  EmojiRatingView.swift
+//  Bookworm
+//
+//  Created by Anmol Chawla on 16/08/24.
+//
+
+import SwiftUI
+
+struct EmojiRatingView: View {
+    let rating: Int
+    
+    var body: some View {
+        switch rating {
+            case 1:
+                Text("😴")
+                
+            case 2:
+                Text("🥱")
+                
+            case 3:
+                Text("😕")
+            
+            case 4:
+                Text("😁")
+            
+            case 5:
+                Text("🤯")
+            
+            default:
+                Text("❓")
+        }
+    }
+}
+
+#Preview {
+    EmojiRatingView(rating: 3)
+}
