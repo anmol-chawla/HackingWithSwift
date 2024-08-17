09a90698f641ac3f8b03e800abdd4580035dd939
Anmol Chawla <anmolchawla10@gmail.com>
2024-08-17 17:19:18 +0530

    Initialize a new project - SwiftDataProject

---
+++ SwiftDataProject/SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Anmol Chawla on 17/08/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
