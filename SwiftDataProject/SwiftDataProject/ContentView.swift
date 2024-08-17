1ec5b82269a2a55895504fb8195847d3bf689b93
Anmol Chawla <anmolchawla10@gmail.com>
2024-08-17 17:47:53 +0530

    Create a new User model and experiment with bindable paths

--- SwiftDataProject/SwiftDataProject/ContentView.swift
+++ SwiftDataProject/SwiftDataProject/ContentView.swift
@@ -5,17 +5,35 @@
 //  Created by Anmol Chawla on 17/08/24.
 //
 
+import SwiftData
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.modelContext) var modelContext
+    
+    @Query(sort: \User.name) var users: [User]
+    
+    @State private var path = [User]()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationStack(path: $path, root: {
+            List(users, rowContent: { user in
+                NavigationLink(value: user, label: {
+                    Text(user.name)
+                })
+            })
+            .navigationTitle("Users")
+            .navigationDestination(for: User.self, destination: { user in
+                EditUserView(user: user)
+            })
+            .toolbar(content: {
+                Button("Add User", systemImage: "plus", action: {
+                    let user = User(name: "", city: "", joinDate: .now)
+                    modelContext.insert(user)
+                    path = [user]
+                })
+            })
+        })
     }
 }
 
