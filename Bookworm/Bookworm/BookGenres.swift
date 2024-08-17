249ff36b8f327939556b556472b49c6eee7557f7
Anmol Chawla <anmolchawla10@gmail.com>
2024-08-17 14:13:48 +0530

    Add a new enum for book genres

---
+++ Bookworm/Bookworm/BookGenres.swift
@@ -0,0 +1,18 @@
+//
+//  BookGenres.swift
+//  Bookworm
+//
+//  Created by Anmol Chawla on 17/08/24.
+//
+
+import Foundation
+
+enum BookGenres: String, CaseIterable, Codable {
+    case Fantasy
+    case Horror
+    case Kids
+    case Mystery
+    case Poetry
+    case Romance
+    case Thriller
+}
