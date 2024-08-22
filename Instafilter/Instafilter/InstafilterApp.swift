6f2d85078ab9bc7a1f09a494b938a7b03b7756a5
Anmol Chawla <anmolchawla10@gmail.com>
2024-08-22 09:53:30 +0530

    Initialize a new project - Instafilter

---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Anmol Chawla on 22/08/24.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
