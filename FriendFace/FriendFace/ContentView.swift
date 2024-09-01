d182dec3c10473c2533b7445feea16df49a3967e
Anmol Chawla <anmolchawla10@gmail.com>
2024-09-01 17:12:18 +0530

    Initialize a new project - FriendFace

---
+++ FriendFace/FriendFace/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FriendFace
+//
+//  Created by Anmol Chawla on 01/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
