c883fa1ca00ec28f3622b6b9f9110f6c4661eb7b
Anmol Chawla <anmolchawla10@gmail.com>
2024-09-02 09:40:38 +0530

    Layout scaffold

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -8,14 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var processedImage: Image?
+    @State private var filterIntensity = 0.5
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationStack(root: {
+            VStack(content: {
+                Spacer()
+                
+                if let processedImage {
+                    processedImage
+                        .resizable()
+                        .scaledToFit()
+                } else {
+                    ContentUnavailableView(
+                        "No picture",
+                        systemImage: "photo.badge.plus",
+                        description: Text("Tap to import a photo")
+                    )
+                }
+                
+                Spacer()
+                
+                HStack(content: {
+                    Text("Intensity")
+                    Slider(value: $filterIntensity)
+                })
+                
+                HStack(content: {
+                    Button("Change Filter", action: changeFilter)
+                    
+                    Spacer()
+                })
+            })
+            .padding([.horizontal, .bottom])
+            .navigationTitle("Instafilter")
+        })
+    }
+    
+    func changeFilter() {
+        
     }
 }
 
