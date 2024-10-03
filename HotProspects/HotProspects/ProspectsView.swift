65c955e9117e983e225fa306250e7ea61c1121cc
Anmol Chawla <anmolchawla10@gmail.com>
2024-10-03 10:23:19 +0530

    Add notifications for reminder alerts

--- HotProspects/HotProspects/ProspectsView.swift
+++ HotProspects/HotProspects/ProspectsView.swift
@@ -8,6 +8,7 @@
 import CodeScanner
 import SwiftData
 import SwiftUI
+import UserNotifications
 
 struct ProspectsView: View {
     enum FilterType {
@@ -43,7 +44,7 @@ struct ProspectsView: View {
             
             _prospects = Query(
                 filter: #Predicate { $0.isContacted == showContactedOnly},
-                 sort: [SortDescriptor(\Prospect.name)]
+                sort: [SortDescriptor(\Prospect.name)]
             )
         }
     }
@@ -73,6 +74,11 @@ struct ProspectsView: View {
                             prospect.isContacted.toggle()
                         }
                         .tint(.green)
+                        
+                        Button("Remind Me", systemImage: "bell") {
+                            addNotification(for: prospect)
+                        }
+                        .tint(.orange)
                     }
                 }
                 .tag(prospect)
@@ -112,7 +118,7 @@ struct ProspectsView: View {
             case .success(let result):
                 let details = result.string.components(separatedBy: "\n")
                 guard details.count == 2 else { return }
-               
+                
                 let person = Prospect(name: details[0], emailAdress: details[1], isContacted: false)
                 modelContext.insert(person)
                 
@@ -124,6 +130,49 @@ struct ProspectsView: View {
     func delete() {
         for prospect in selectedProspects {
             modelContext.delete(prospect)
+        }
+    }
+    
+    func addNotification(for prospect: Prospect) {
+        let center = UNUserNotificationCenter.current()
+        
+        let addRequest = {
+            let content = UNMutableNotificationContent()
+            content.title = "Contact \(prospect.name)"
+            content.subtitle = prospect.emailAdress
+            content.sound = UNNotificationSound.default
+            
+            var dateComponents = DateComponents()
+            dateComponents.hour = 9
+            
+//            let trigger = UNCalendarNotificationTrigger(
+//                dateMatching: dateComponents,
+//                repeats: false
+//            )
+            
+            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+            
+            let request = UNNotificationRequest(
+                identifier: UUID().uuidString,
+                content: content,
+                trigger: trigger
+            )
+        
+            center.add(request)
+        }
+        
+        center.getNotificationSettings { settings in
+            if settings.authorizationStatus == .authorized {
+                addRequest()
+            } else {
+                center.requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+                    if success {
+                        addRequest()
+                    } else if let error {
+                        print(error.localizedDescription)
+                    }
+                }
+            }
         }
     }
 }
