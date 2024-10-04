c82370a4ae04811baa5807bf793438e62992e8d9
Anmol Chawla <anmolchawla10@gmail.com>
2024-10-04 10:01:20 +0530

    HotProspects (#9)
    * Initialize a new project - HotProspects
    
    * Create a tab view with sample pages
    
    * Create a prospect class to store lead information
    
    * Sort prospects based on filter type
    
    * Add generation of qr code for me view
    
    * Add QR scanning package to scan and add prospect info
    
    * Add swipe actions for prospects view
    
    * Implement sharing for qr code
    
    * Add notifications for reminder alerts

---
+++ HotProspects/HotProspects/MeView.swift
@@ -0,0 +1,71 @@
+//
+//  MeView.swift
+//  HotProspects
+//
+//  Created by Anmol Chawla on 01/10/24.
+//
+
+import CoreImage.CIFilterBuiltins
+import SwiftUI
+
+struct MeView: View {
+    @AppStorage("name") private var name = "Anonymous"
+    @AppStorage("emailAddress") private var emailAddress = "you@test.com"
+    
+    @State private var qrCode = UIImage()
+    
+    let context = CIContext()
+    let filter = CIFilter.qrCodeGenerator()
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                TextField("Name", text: $name)
+                    .textContentType(.name)
+                    .font(.title)
+                
+                TextField("Email address", text: $emailAddress)
+                    .textContentType(.emailAddress)
+                    .font(.title)
+                
+                Image(uiImage: qrCode)
+                    .interpolation(.none)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height:  200)
+                    .contextMenu {
+                        ShareLink(
+                            item: Image(uiImage: qrCode),
+                            preview: SharePreview("My QR Code", image: Image(uiImage: qrCode))
+                        )
+                    }
+            }
+            .navigationTitle("Your code")
+            .onAppear(perform: updateQRCode)
+            .onChange(of: name, updateQRCode)
+            .onChange(of: emailAddress, updateQRCode)
+        }
+    }
+    
+    func updateQRCode() {
+        qrCode = generateQRCode(from: "\(name)\n\(emailAddress)")
+    }
+    
+    func generateQRCode(from string: String) -> UIImage {
+        filter.message = Data(string.utf8)
+        
+        if let outputImage = filter.outputImage {
+            if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
+                qrCode = UIImage(cgImage: cgImage)
+                
+                return qrCode
+            }
+        }
+        
+        return UIImage(systemName: "xmark.circle") ?? UIImage()
+    }
+}
+
+#Preview {
+    MeView()
+}
---
+++ HotProspects/HotProspectsTests/HotProspectsTests.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsTests.swift
+//  HotProspectsTests
+//
+//  Created by Anmol Chawla on 24/09/24.
+//
+
+import Testing
+@testable import HotProspects
+
+struct HotProspectsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
