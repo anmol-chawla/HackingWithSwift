727c01024154d9442ce1201e77ba6f1f57be09f7
Anmol <anmol@dashtoon.com>
2024-11-01 15:11:17 +0530

    chore: initialize a new project - SnowSeeker

---
+++ SnowSeeker/SnowSeeker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SnowSeeker
+//
+//  Created by Anmol Chawla on 01/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SnowSeeker/SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Anmol Chawla on 01/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
