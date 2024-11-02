7157be65bee4771f5fbb699ee4fc7b5e519d3de2
Anmol <anmol@dashtoon.com>
2024-11-02 13:25:10 +0530

    chore: create a resort struct

---
+++ SnowSeeker/SnowSeeker/Resort.swift
@@ -0,0 +1,22 @@
+//
+//  Resort.swift
+//  SnowSeeker
+//
+//  Created by Anmol Chawla on 02/11/24.
+//
+
+import Foundation
+
+struct Resort: Codable, Hashable, Identifiable {
+    var id: String
+    var name: String
+    var country: String
+    var description: String
+    var imageCredit: String
+    var price: Int
+    var size: Int
+    var snowDepth: Int
+    var elevation: Int
+    var runs: Int
+    var facilities: [String]
+}
