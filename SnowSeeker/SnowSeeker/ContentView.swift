77d6107f2675488b7f974529aa3394f7198b68e3
Anmol <anmol@dashtoon.com>
2024-11-02 13:54:40 +0530

    feat: initial split view

--- SnowSeeker/SnowSeeker/ContentView.swift
+++ SnowSeeker/SnowSeeker/ContentView.swift
@@ -8,14 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    let resorts: [Resort] = Bundle.main.decode("resorts.json")
+ 
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationSplitView {
+            List(resorts) { resort in
+                NavigationLink(value: resort) {
+                    HStack {
+                        Image(resort.country)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 40, height: 25)
+                            .clipShape(.rect(cornerRadius: 5))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 5)
+                                    .stroke(.black, lineWidth: 1)
+                            )
+                        
+                        VStack(alignment: .leading) {
+                            Text(resort.name)
+                                .font(.headline)
+                            
+                            Text("\(resort.runs) runs")
+                                .foregroundStyle(.secondary)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Resorts")
+        } detail: { Text("Detail") }
     }
 }
 
