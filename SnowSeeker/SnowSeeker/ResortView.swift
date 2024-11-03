af828ba2b6a03f9bc2cad4c3e9fec0cfa7e7c20a
Anmol <anmol@dashtoon.com>
2024-11-03 12:23:39 +0530

    feat: create a component for showing resort pricing and size

---
+++ SnowSeeker/SnowSeeker/ResortView.swift
@@ -0,0 +1,18 @@
+//
+//  ResortView.swift
+//  SnowSeeker
+//
+//  Created by Anmol Chawla on 02/11/24.
+//
+
+import SwiftUI
+
+struct ResortView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ResortView()
+}
